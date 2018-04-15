7997aa24760beb51669cc9744298f3ca6e035e56
Liz <ehwaz.liz@gmail.com>
2018-04-15 17:46:27 -0500

    Actualizada--

--- instructionMemory.vhd
+++ instructionMemory.vhd
@@ -1,35 +1,9 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:26:38 10/28/2012 
--- Design Name: 
--- Module Name:    instructionMemory - arqInstructionMemory 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use std.textio.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity instructionMemory is
     Port ( 
@@ -41,34 +15,30 @@ end instructionMemory;
 
 architecture arqInstructionMemory of instructionMemory is
 
-	type rom_type is array (0 to 63) of std_logic_vector (31 downto 0);
-		
-	impure function InitRomFromFile (RomFileName : in string) return rom_type is
-		FILE RomFile : text open read_mode is RomFileName;
-		variable RomFileLine : line;
-		variable temp_bv : bit_vector(31 downto 0);
-		variable temp_mem : rom_type;
-		begin
-			for I in rom_type'range loop
-				readline (RomFile, RomFileLine);
-				read(RomFileLine, temp_bv);
-				temp_mem(i) := to_stdlogicvector(temp_bv);
-			end loop;
-		return temp_mem;
-	end function;
-	
-	signal instructions : rom_type := InitRomFromFile("testJMPL.data");
+type rom_type is array(0 to 63) of bit_vector(31 downto 0);    
+impure function prueba(p : in string) return rom_type is                                                         
+        FILE q        : text is in p;                       
+        variable z : line;                                 
+        variable ROM      : rom_type;                                      
+     begin                                                        
+        for I in rom_type'range loop                                  
+            readline (q, z);                               
+            read (z, ROM(I));                                 
+        end loop;                                                    
+        return ROM;                                                  
+     end function;                                                
+ 
+signal ROM : rom_type := prueba("codigoTXT.data.txt");
 	
 begin
---reset,address, instructions)
-	process(reset,address, instructions)--clk)
+	process(reset,address,ROM)
 	begin
-		--if(rising_edge(clk))then
-			if(reset = '1')then
-				outInstruction <= (others=>'0');
-			else
-				outInstruction <= instructions(conv_integer(address(5 downto 0)));
-			end if;
-		--end if;
+		if(reset = '1') then
+			outInstruction <= (others => '0');
+			ROM <= (others => X"00000000");
+		else
+			outInstruction <= to_stdlogicvector(ROM(conv_integer(address(5 downto 0))));
+		end if;
 	end process;
+
 end arqInstructionMemory;
